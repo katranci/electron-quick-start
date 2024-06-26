205d3aac0f3bbcaaf36ff5b164281cd4b30a251c
Ahmet Katranci <ahmet@katranci.co.uk>
2024-06-26 13:09:58 +0100

    Replace user-agent string

---
+++ constants.js
@@ -0,0 +1,13 @@
+const USERAGENT = (() => {
+  if (process.platform === "win32") {
+    return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36";
+  } else if (process.platform === "darwin") {
+    return "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36";
+  } else {
+    return "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36";
+  }
+})();
+
+module.exports = {
+  USERAGENT,
+};
